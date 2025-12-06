292faf3078de15a4a7f239130265eca4b6942543
Erick González <erickgoonzalez5@gmail.com>
2025-12-05 23:07:28 -0600

    Agregue diseño de la ventana Altas medicos con sus archivos Activity su layout y codigo para q se ejecute visualmente

--- app/src/main/java/com/example/pf_room_bd_farmacias/ActivityAltasMedicos.java
+++ app/src/main/java/com/example/pf_room_bd_farmacias/ActivityAltasMedicos.java
@@ -2,6 +2,8 @@ package com.example.pf_room_bd_farmacias;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.annotation.Nullable;
 
@@ -10,5 +12,24 @@ public class ActivityAltasMedicos extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_altas_medicos);
+
+
+        Spinner spinnerEspecialidad;
+        ArrayAdapter<CharSequence> adapter;
+
+        spinnerEspecialidad = findViewById(R.id.spinnerEspecialidad);
+
+        adapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.especialidades_array,
+                android.R.layout.simple_spinner_item
+        );
+
+// diseño del desplegable
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+// conectar el adapter
+        spinnerEspecialidad.setAdapter(adapter);
+
     }
 }
--- app/src/main/java/com/example/pf_room_bd_farmacias/ActivityMedicos.java
+++ app/src/main/java/com/example/pf_room_bd_farmacias/ActivityMedicos.java
@@ -1,7 +1,9 @@
 package com.example.pf_room_bd_farmacias;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 
@@ -10,5 +12,13 @@ public class ActivityMedicos extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio_medicos);
+    }
+
+    public void abrirActivitiesMedico(View view){
+        Intent i = null;
+        if (view.getId() == R.id.btnAltasMedicos){
+            i = new Intent(this, ActivityAltasMedicos.class);
+        }
+        startActivity(i);
     }
 }
